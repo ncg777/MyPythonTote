6042ab8ebd6535c912dcb151b72ac1414a906294
Nicolas Couture-Grenier <nicolas.couture.grenier@gmail.com>
2024-04-13 10:35:40 -0400

    NecklaceGenerator

---
+++ NecklacesGenerator.py
@@ -0,0 +1,24 @@
+
+def _subGen(t, p, n, k, array, necklaces):
+        if t > n:
+            if (n % p) == 0:
+                tmpArray = [0] * n
+                for i in range(n):
+                    tmpArray[i] = array[i + 1]
+                necklaces.append(tmpArray)
+        else:
+            array[t] = array[t - p]
+            _subGen(t+1, p, n, k, array, necklaces)
+            for j in range(array[t - p] + 1, k):
+                array[t] = j
+                _subGen(t+1, t, n, k, array, necklaces)
+
+def generate(n, k):
+    necklaces = []
+    array = [0] * (n+1)
+    _subGen(1, 1, n, k, array, necklaces)
+    return necklaces
+
+if __name__ == "__main__":
+    x=GenerateNecklaces(12,2)
+    print(len(x))
